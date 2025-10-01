d424c6f033bb5030b122738c6433f39b20e492db
TB-PC <tbdlarsen@gmail.com>
2025-10-01 22:48:21 +0200

    neural network for mi 4 task 1

--- mi/NN.py
+++ mi/NN.py
@@ -1,26 +1,43 @@
 import math
 
-input = [[1,5],[0,14]]
-weight_1 = [[1.3,-0.3],[1.7,1]]
-weight_2 = [0.6,-1.4]
-input_iteration = 0
-hidden = [0,0]
 
 def sig(x):
     return 1/(1+math.exp(-x))
 
-result = 0
-for i in range(2):
-    for j in range(2):
-        hidden[i] += input[input_iteration][j]*weight_1[i][j]
-    
-    hidden[i] = sig(hidden[i])
 
-for i in range(2):
-    result += hidden[i]*weight_2[i]
-result = sig(result)
+layers = [
+    [1,5],
+    [],
+    []
+
+]
+weights = [
+   [[1.3,1.7],[-0.3,1.0]],
+   [[0.6,-1.4]]
+]
 
 
-print(result)
+
+for layer in range(1, len(layers)):
+    prev_layer = layers[layer-1]
+    layer_weights = weights[layer-1]
+    current_layer_outputs = []
+
+    num_nodes_this_layer = len(layer_weights)
+
+    for node in range(num_nodes_this_layer):
+        node_weights = layer_weights[node]
+        node_input = 0
+        for prev_node in range(len(prev_layer)):
+            node_input += prev_layer[prev_node]*node_weights[prev_node]
+        node_output = sig(node_input)
+        current_layer_outputs.append(node_output)
+    layers[layer] = current_layer_outputs
+
+
+print(layers)
+
+
+
 
 

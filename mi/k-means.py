ebdd4ef852d7d5427087fefbae5b022b7f615541
TB-PC <tbdlarsen@gmail.com>
2025-10-07 21:43:59 +0200

    assign datapoints and find closest cluster functions

--- mi/k-means.py
+++ mi/k-means.py
@@ -11,11 +11,29 @@ def initial_clusters(amount_of_clusters):
         clusters.append([])
     return clusters
 
-def euclidian_dist(x,y):
+def euclidian_dist(x,y) -> float:
     dist = 0
     for i in range(len(x)):
         dist += (x[i]-y[i])**2
     return dist**(1/2)
+
+def get_closest_cluster(amount_of_clusters, centers, data_point):
+    distance = sys.float_info.max
+    closest = 0
+    for i in range(amount_of_clusters):
+        new_distance = euclidian_dist(data_point,centers[i])
+        if(new_distance < distance):
+            distance = new_distance
+            closest = i
+    return closest
+
+def assign_datapoints(data_points, centers, amount_of_clusters, clusters):
+    for data_point in data_points:
+        closest_cluster = get_closest_cluster(amount_of_clusters, centers, data_point)
+        clusters[closest_cluster].append(data_point)
+    return clusters
+
+
 
 k = 2
 
@@ -24,19 +42,22 @@ dimension = len(S[1])
 
 clusters = initial_clusters(k)
 centers = initial_centers(dimension,S)
+clusters = assign_datapoints(S,centers,k,clusters)
 
-dist = sys.float_info.max
-closest = 0
-for i in range(k):
-    new_dist = euclidian_dist(S[0],centers[i])
-    if(new_dist < dist):
-        dist = new_dist
-        closest = i
+new_centers = initial_centers(dimension,S)
 
 print(clusters)
-clusters[closest].append(S[0])
+for i in range(len(clusters[0])):
+    
+    new_centers[0] += clusters[0][i]
+print(new_centers)
+new_centers[0] /= len(clusters[0])
+
+
+
+
+
 
 
 
 print(centers)
-print(clusters)

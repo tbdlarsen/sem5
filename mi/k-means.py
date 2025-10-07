e2ef952542f443878e1dbdb489fc3102aecc2d74
TB-PC <tbdlarsen@gmail.com>
2025-10-07 23:14:03 +0200

    finished k-means algorithm

--- mi/k-means.py
+++ mi/k-means.py
@@ -1,20 +1,18 @@
 import sys
 def initial_centers(k, data):
-    init = []
-    for i in range(k):
-        init.append(data[i])
-    return init
+    
+    return data[:k]
 
 def initial_clusters(amount_of_clusters):
-    clusters = []
-    for i in range(amount_of_clusters):
-        clusters.append([])
+    clusters = [[] for _ in range(amount_of_clusters)]
+
     return clusters
 
 def euclidian_dist(x,y):
     dist = 0
     for i in range(len(x)):
         dist += (x[i]-y[i])**2
+
     return dist**(1/2)
 
 def get_closest_cluster(amount_of_clusters, centers, data_point):
@@ -25,13 +23,16 @@ def get_closest_cluster(amount_of_clusters, centers, data_point):
         if(new_distance < distance):
             distance = new_distance
             closest = i
+
     return closest
 
 def assign_datapoints(data_points, centers, amount_of_clusters, clusters):
     for data_point in data_points:
         closest_cluster = get_closest_cluster(amount_of_clusters, centers, data_point)
         clusters[closest_cluster].append(data_point)
+
     return clusters
+
 
 def get_new_center(cluster,dimension):
     new_center = []
@@ -42,6 +43,7 @@ def get_new_center(cluster,dimension):
         new_center[dim] /= len(cluster)
 
     return new_center
+
 
 def init_new_centers(dimension, amount_of_clusters):
     new_centers = []
@@ -59,46 +61,43 @@ def new_centers(clusters, dimension):
         centers[i] = get_new_center(clusters[i],dimension)
     return centers
 
-def center_difference(old_centers, new_centers):
+
+def center_difference(old_centers, curr_centers):
     tolerance = 1e-6
-    for old,new in zip(old_centers,new_centers):
+    for old,new in zip(old_centers,curr_centers):
         if (euclidian_dist(old,new) > tolerance):
             return True
     return False
 
-k = 2
 
-S = [[1,2],[3,4],[1.2,3.4]]
-dimension = len(S[0])
+def iterate_k_means(centers,k,S,dimension):
+    next_centers = None
+    while(not(next_centers) or center_difference(centers,next_centers) ):
+        if(next_centers):
+            centers = next_centers
+        clusters = initial_clusters(k)
+        clusters = assign_datapoints(S,centers,k,clusters)
+        next_centers= new_centers(clusters,dimension)
+    return(centers, clusters)
 
-clusters = initial_clusters(k)
-centers = initial_centers(k,S)
-clusters = assign_datapoints(S,centers,k,clusters)
-next_centers= new_centers(clusters,dimension)
 
 
-print(clusters)
-print(centers)
-print()
+def main():
 
-while(center_difference(centers,next_centers)):
-    clusters = initial_clusters(k)
-    centers = next_centers
-    clusters = assign_datapoints(S,centers,k,clusters)
-    next_centers= new_centers(clusters,dimension)
+    k = 2
+    S = [[1,2],[3,4],[1.2,3.4]]
+    dimension = len(S[0])
+    centers = initial_centers(k,S)
 
+    centers, clusters = iterate_k_means(centers, k, S,dimension)
+
+    print("final")
     print(clusters)
     print(centers)
     print()
-
-
-
-print("final")
-print(clusters)
-print(centers)
-print()
-        
-
+            
+if __name__ == "__main__":
+    main()
 
 
 

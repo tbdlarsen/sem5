4db7db3d977652026af159a86b02f320436a68da
TB-PC <tbdlarsen@gmail.com>
2025-10-08 21:53:58 +0200

    fixed revise function to include all constraints for variable pairs

--- mi/csp/csp.py
+++ mi/csp/csp.py
@@ -1,24 +1,32 @@
 
 import inspect
 
-def revise(gamma, u, v, D, constraint):
-    for i in D[u]:
-       if not any(constraint(i,j) for j in D[v]):
+def revise(gamma, u, v, constraints):
+    D = gamma[1]
+    for i in D[u][:]:
+       if not any(all(constraint(i,j) for constraint in constraints) for j in D[v]):
         D[u].remove(i)
-
-
     return gamma
 
 
 V = {'v0','v1'}
 D = {
-    'v0':[1,2],
-    'v1':[1]
+    'v0':[1,2,3,4],
+    'v1':[1,2,3]
 }
 
 C = {
-    ('v0','v1'): lambda v0,v1: v0!=v1
+    ('v0','v1'): [
+        lambda v0,v1: v0 != v1,
+        lambda v0,v1: v0 < v1
+    ]
+    
 }
 gamma = (V,D,C)
-gamma = revise(gamma, 'v0','v1', D, C[('v0','v1')])
+
+for variables, constraint_func in gamma[2].items():
+    var1, var2 = variables
+    gamma = revise(gamma, var1, var2,constraint_func)
+
+
 print(gamma)

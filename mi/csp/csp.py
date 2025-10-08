67e2674181ff9b1cb1409f39884c22664c4b675c
TB-PC <tbdlarsen@gmail.com>
2025-10-08 22:15:56 +0200

    added todos

--- mi/csp/csp.py
+++ mi/csp/csp.py
@@ -4,9 +4,12 @@ import inspect
 def revise(gamma, u, v, constraints):
     D = gamma[1]
     for i in D[u][:]:
-       if not any(all(constraint(i,j) for constraint in constraints) for j in D[v]):
-        D[u].remove(i)
+        if not any(all(constraint(i,j) for constraint in constraints) for j in D[v]):
+            D[u].remove(i)
     return gamma
+
+
+
 
 
 V = {'v0','v1'}
@@ -30,3 +33,11 @@ for variables, constraint_func in gamma[2].items():
 
 
 print(gamma)
+
+
+#todo Arc consistency
+#todo Backtracking search
+#todo Inference
+#todo Variable elimination
+#todo constraint graph
+#

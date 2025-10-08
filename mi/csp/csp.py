5ff7441bf37f875d3e3ae4b60517a3f7b1423334
TB-PC <tbdlarsen@gmail.com>
2025-10-08 21:38:06 +0200

    revise function for CSP

--- mi/csp/csp.py
+++ mi/csp/csp.py
@@ -1,14 +1,24 @@
 
-V = {'A','B'}
+import inspect
+
+def revise(gamma, u, v, D, constraint):
+    for i in D[u]:
+       if not any(constraint(i,j) for j in D[v]):
+        D[u].remove(i)
+
+
+    return gamma
+
+
+V = {'v0','v1'}
 D = {
-    'A':[1,2],
-    'B':[1,2]
+    'v0':[1,2],
+    'v1':[1]
 }
 
 C = {
-    ('A','B'): lambda a,b: a!=b,
-    ('A','B'): lambda a,b: a<b
+    ('v0','v1'): lambda v0,v1: v0!=v1
 }
-
 gamma = (V,D,C)
-#print(gamma)
+gamma = revise(gamma, 'v0','v1', D, C[('v0','v1')])
+print(gamma)
